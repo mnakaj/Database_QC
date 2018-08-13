4b9c90001cc16db77ba92aab0de1064df0aa24c9
Mia Nakajima <mia.nakajima@berkeley.edu>
2018-08-13 16:36:59 -0700

    Delete funcs.R

--- funcs.R
+++
@@ -1,9 +0,0 @@
-check_contributor <- function(contributor){
-  
-  if (sum(grepl("[[:digit:]]", contributor)) > 0){
-    print("There seems to be a name with a possible typo. Please check and then proceed.")
-  }else{
-    print("Ok.")
-  }
-  
-}
